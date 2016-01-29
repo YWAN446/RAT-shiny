9ae3a4f475ae2ca8481f5c93792d5aa4f7f3fd7e
Aaron White <aaroncwhite@gmail.com>
2016-01-29 22:21:49 +0000

    api helpers

--- api_helpers.R
+++ api_helpers.R
@@ -1,4 +1,31 @@
-## API Helpers
+## API Helpers -------------------------------------------------------------
+# These functions are designed to help the Shiny Server interact with 
+# its FormHub counterpart across the web.  This is currently over an 
+# unecrypted port, and something for which we should look into setting better 
+# security
+# FormHub API documentation can be accessed at:
+# https://formhub.org/api/v1/
 
-api_token <- '8d0336d37ef28df590574f1cd4531f142e31ca02'
 
+# TOKEN --------------------------------------------------------------------
+# Available by logging into FormHub, selecting the user account in the top right
+# and selecting "API Key"
+
+# This is specific to the SP user at the moment
+api_token <- '8d0336d37ef28df590574f1cd4531f142e31ca02' 
+
+formhubGET <- function(api_)
+
+getAPI_forms <- function(api_url, api_token) {
+  # This accesses the /api/v1/data access point and downloads
+  # the available forms that the user has created and the appropriate
+  # web links for downloading data.  We'll output a dataframe with 
+  # two columns, formName, apiLink.
+  # Ex. 
+  # > getAPI_forms('http://formhub.cgsw.org/', api_token)
+  # formName    apiLink
+  # community   /api/v1/data/sp/92
+  
+  response
+  
+}
