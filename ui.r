9369cdbf87589dd4f625513b33634bf0fab9c01a
Aaron White <aaroncwhite@gmail.com>
2016-04-30 17:34:12 +0000

    cleaned up some old code and added progress bar for bayesian analyses

--- ui.r
+++ ui.r
@@ -13,49 +13,49 @@ shinyUI(fluidPage(theme = "bootstrap.css",
   padding: 20px"),
   br(),
 #  titlePanel("SaniPath"),
+  sidebarLayout(
+    sidebarPanel(
+      h3('Analysis Settings:'),
+      selectInput('surtype','Survey Type', selected="0", choices=c("All"=0,"Household"=1,"School"=2,"Community"=3)),
+      hr(),
+      h4('Order Results:'),
+      selectizeInput('level1', 'Level 1', choices=c('Sample' = 'sample', 'Neighborhood' = 'neighborhood', 'Age' = 'age'), selected='sample'),
+      selectInput('level2', 'Level 2', choices=c(), selected=''),
+      selectInput('level3', 'Level 3', choices=c(), selected=''),
+      
+      
+      tags$b(textOutput('plot_display')),
+      # actionButton('make_pie', 'Make pies'),
+      fluidRow(
+        column(6,
+               checkboxGroupInput('sample', 'Sample Choices:', choices="", selected="")
+               ),
+        column(6,
+               checkboxGroupInput('age', 'Age Choices:',choices="", selected="")
+               
+               )
+        
+      ),
+      
+      checkboxGroupInput('neighborhood', 'Neighborhood Choices:',choices="", selected=""),
+      selectInput('samtype', 'Sample Type', choices = c("All"=0,c("Drain Water"=1, "Produce"=2, "Piped Water"=3, 
+                                                                          "Ocean Water"=4, "Surface Water"=5, "Flood Water"=6,
+                                                                          "Public Latrine Surfaces"=7, "Particulate"=8, "Bathing"=9))),
+
+      selectInput('neighb', 'Neighborhood', choices=""),
+      selectInput('ad_ch','Adults or Children',choices=c("All"=0,"Adults"=1,"Children"=2)),
+      radioButtons('format', 'Document format', c('PDF', 'HTML', 'Word'),
+                   inline = TRUE),
+      downloadButton('downloadReport'),
+      br(),
+      br(),
+      br(),
+      img(src="sanipath_logo.png", height = 80, width = 250)
+    ),
+    mainPanel(
       tabsetPanel(type = "tabs", 
                   tabPanel("Pie Chart", 
-                           sidebarLayout(
-                             sidebarPanel(
-                               h3('Analysis Settings:'),
-                               selectInput('surtype','Survey Type', selected="0", choices=c("All"=0,"Household"=1,"School"=2,"Community"=3)),
-                               hr(),
-                               h4('Order Results:'),
-                               selectizeInput('level1', 'Level 1', choices=c('Sample' = 'sample', 'Neighborhood' = 'neighborhood', 'Age' = 'age'), selected='sample'),
-                               selectInput('level2', 'Level 2', choices=c(), selected=''),
-                               selectInput('level3', 'Level 3', choices=c(), selected=''),
-                               
-                               
-                               tags$b(textOutput('plot_display')),
-                               # actionButton('make_pie', 'Make pies'),
-                               fluidRow(
-                                 column(6,
-                                        checkboxGroupInput('sample', 'Sample Choices:', choices="", selected="")
-                                 ),
-                                 column(6,
-                                        checkboxGroupInput('age', 'Age Choices:',choices="", selected="")
-                                        
-                                 )
-                                 
-                               ),
-                               
-                               checkboxGroupInput('neighborhood', 'Neighborhood Choices:',choices="", selected=""),
-                               selectInput('samtype', 'Sample Type', choices = c("All"=0,c("Drain Water"=1, "Produce"=2, "Piped Water"=3, 
-                                                                                           "Ocean Water"=4, "Surface Water"=5, "Flood Water"=6,
-                                                                                           "Public Latrine Surfaces"=7, "Particulate"=8, "Bathing"=9))),
-                               
-                               selectInput('neighb', 'Neighborhood', choices=""),
-                               selectInput('ad_ch','Adults or Children',choices=c("All"=0,"Adults"=1,"Children"=2)),
-                               radioButtons('format', 'Document format', c('PDF', 'HTML', 'Word'),
-                                            inline = TRUE),
-                               downloadButton('downloadReport'),
-                               br(),
-                               br(),
-                               br(),
-                               img(src="sanipath_logo.png", height = 80, width = 250)
-                             ),
-                             mainPanel(
-                           uiOutput("pie_charts"))),
+                           uiOutput("pie_charts")),
                   tabPanel("Histogram", uiOutput('histograms')),
                   tabPanel("People Plot", uiOutput('ppl_plots')),
                   tabPanel("Raw Data", 
@@ -84,6 +84,6 @@ shinyUI(fluidPage(theme = "bootstrap.css",
                   
                 )
       
-    
+    )
   )
 ))
