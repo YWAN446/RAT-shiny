c3a52ce6c07e919ae33566f24642e28a835b9661
Aaron White <aaroncwhite@gmail.com>
2016-07-12 01:11:22 +0000

    added global config file

--- global.R
+++ global.R
@@ -8,9 +8,9 @@ pwd <- 'password'
 
 ## Load the files we need
 
-source("./model/PS_Plot.r")
+source("model/PS_Plot.r")
 options(shiny.maxRequestSize = 9*1024^2)
-source('api_helpers.R') # Formhub interactions
-source('analysis_helpers.R') # Number crunching
-source('plotting_helpers.R') # Number presenting
-source('report_helpers.R') # Specifically for reporting
+source('model/api_helpers.R') # Formhub interactions
+source('model/analysis_helpers.R') # Number crunching
+source('model/plotting_helpers.R') # Number presenting
+source('model/report_helpers.R') # Specifically for reporting
