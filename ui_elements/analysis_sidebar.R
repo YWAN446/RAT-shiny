bb5162dd36e3b79b4d5fcabb1cf9e5e38caceac8
Aaron White <aaroncwhite@gmail.com>
2016-10-06 21:49:44 +0000

    code cleanup

--- ui_elements/analysis_sidebar.R
+++ ui_elements/analysis_sidebar.R
@@ -4,13 +4,7 @@ analysis_sidebar <- fluidRow(
     actionButton("update_forms", "Refresh Data", icon=icon('refresh')),
     helpText("Press to download new data from Formhub or use uploaded CSVs to generate 
              analysis figures."),
-    
-    #       selectInput('samtype', 'Sample Type', choices = c("All"=0,c("Drain Water"=1, "Produce"=2, "Piped Water"=3, 
-    #                                                                           "Ocean Water"=4, "Surface Water"=5, "Flood Water"=6,
-    #                                                                           "Public Latrine Surfaces"=7, "Particulate"=8, "Bathing"=9))),
-    # 
-    #       selectInput('neighb', 'Neighborhood', choices=""),
-    #       selectInput('ad_ch','Adults or Children',choices=c("All"=0,"Adults"=1,"Children"=2)),
+
     radioButtons('format', 'Document format', c('PDF', 'HTML', 'Word'),
                  inline = TRUE),
     downloadButton('downloadReport'),
