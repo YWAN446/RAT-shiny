e508f2549ea59e6789a78824bc2c8181f9f2e466
Aaron White <aaroncwhite@gmail.com>
2017-11-19 22:58:38 -0500

    uncommented lines

--- python/sp_tests.py
+++ python/sp_tests.py
@@ -14,22 +14,21 @@ lab = pd.read_csv('rsrc/LAB_EXAMPLE.csv')
 # Example with school data collected
 
 # Frequency data are used with pie charts
-# sc_freq = analysis.compute_frequencies(sc)
-# sc_freq = analysis.make_plots(sc_freq, 'pie')
-#
-# # concentration data are used with histograms
-# conc = analysis.compute_concentrations(col, lab)
-# conc = analysis.make_plots(conc, 'hist')
-#
-# # Exposure data uses people plots
-# # we also need to make the frequencies again but for people plots
-# sc_freq_ppl = analysis.compute_frequencies(sc, type='ppl', survey_type='school')
-# # we can use the same concentration data already calculated
-# ppl_plots = analysis.compute_exposure(sc_freq_ppl, conc)
-# ppl_plots = analysis.make_plots(ppl_plots)
+sc_freq = analysis.compute_frequencies(sc)
+sc_freq = analysis.make_plots(sc_freq, 'pie')
+
+# concentration data are used with histograms
+conc = analysis.compute_concentrations(col, lab)
+conc = analysis.make_plots(conc, 'hist')
+
+# Exposure data uses people plots
+# we also need to make the frequencies again but for people plots
+sc_freq_ppl = analysis.compute_frequencies(sc, type='ppl', survey_type='school')
+# we can use the same concentration data already calculated
+ppl_plots = analysis.compute_exposure(sc_freq_ppl, conc)
+ppl_plots = analysis.make_plots(ppl_plots)
 
 
-import pdb; pdb.set_trace()
 # the analysis.config object has default values for pathway labels and codes
 # but it's better to explicitly pass the values from SP tool as dictionaries converted to R lists/vectors
 # IMPORTANT: Note that this will reduce the number of analyses performed since the R functions only look for
