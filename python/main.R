4d6825e6089af3f7d2129425d83ffe0a7059361c
Aaron White <aaroncwhite@gmail.com>
2017-12-03 23:57:51 -0500

    removed intake rda.  just leaving it in config for now.

--- python/main.R
+++ python/main.R
@@ -1,7 +1,7 @@
 # Main functions for SaniPath analysis and plotting functions
 sapply(grep('.R$', list.files('model', full.names = T), value=T), source)
 load("rsrc/mpn_tbl.rda")
-load("rsrc/intake.rda")
+#load("rsrc/intake.rda")
 source('config.R')
 
 
