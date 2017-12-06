a2bb9513e00b7ec021e230e953efaf8c01408504
Aaron <aaroncwhite@gmail.com>
2017-12-05 22:36:23 -0500

    updated utilities to generate requirements file

--- python/model/utilities.R
+++ python/model/utilities.R
@@ -24,3 +24,46 @@ fname <- function(dir, plot_type, fn) {
   # returns filename with png appended
   return(sprintf('%s%s_%s', dir, plot_type, fn))
 }
+
+build_requirements_file <- function(base_directory='./', out_dir = './', out_name = 'r-requirements.txt') {
+  # Read lines from R or r files and find any time "library(.)" occurs. 
+  # strip the library lines out and make a requirements file commented
+  # by file name
+  files <- grep('\\.R$|\\.r$|\\.Rmd', list.files(base_directory, recursive = T), value=T)
+  f <- file(file.path(out_dir, out_name))
+
+  libraries <- lapply(files, function(x) {
+    x_lines <- readLines(x)
+    x_lines <- unlist(strsplit(x_lines, ' '))
+    libs <- grep('library\\(.*\\)', x_lines, value=T, perl=T)
+    libs <- gsub('library\\(|\\)', '', libs, perl = T)
+    libs <- libs[libs != "\".\""]
+    # cat(sprintf("# %s", x), libs, file= f, sep='\n', append=T)
+    paste(c(sprintf("# %s", x), libs), collapse="\n")
+  })
+  
+  libraries <- paste(libraries, collapse="\n\n")
+  cat('# R Requirements File',
+      sprintf('# Generated %s', Sys.Date()),
+      "# Use install_requirements() to install",
+      "# _____________________________________",
+      "", 
+      libraries,
+      file= f, sep='\n', append=T)
+  close(f)
+  return(libraries)
+}
+
+install_requirements <- function(requirements_file, only_missing=T) {
+  # Install requirements listed in an file of libraries
+  # one per line
+  reqs <- readLines(requirements_file)
+  reqs <- reqs[!grepl("#", reqs) & reqs != ""]
+  
+  if (only_missing) {
+    reqs = reqs[!(reqs %in% installed.packages())]
+  }
+  
+  install.packages(reqs)
+  
+}
